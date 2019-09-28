a3033c904ae04a33d45f2809682e6657a7f1811c
alrehamy <alrehamyhh@cardiff.ac.uk>
2019-09-28 18:10:05 +0300

    Add files via upload

---
+++ OpenNLP/TaggedWord.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenNLP
+{
+    /// <summary>
+    /// A word tagged by a part of speech tagger
+    /// ex: will/MD | to/TO | etc.
+    /// </summary>
+    public class TaggedWord
+    {
+        public string Tag { get; set; }
+        public string Word { get; set; }
+        public int Index { get; set; }
+        public  WordNetApi.Core.SynSet Sense { get; set; }
+
+        /// <summary>
+        /// Constructor for string of format "will/MD"
+        /// </summary>
+        public TaggedWord(string stringTaggedWord, int indexInGroup)
+        {
+            if (stringTaggedWord.Contains("/"))
+            {
+                this.Word = stringTaggedWord.Split('/').First();
+                this.Tag = stringTaggedWord.Split('/').Last();
+                this.Index = indexInGroup;
+                this.Sense = null;
+            }
+        }
+
+        public TaggedWord(string word, string tag, int index)
+        {
+            this.Word = word;
+            this.Tag = tag;
+            this.Index = index;
+            this.Sense = null;
+        }
+
+        public override string ToString()
+        {
+            return this.Word+"/"+this.Tag;
+        }
+    }
+}
